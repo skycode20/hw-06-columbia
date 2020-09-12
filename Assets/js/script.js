ae9b99f671773a6d006658229e6be83bfef35d21
Skyler R <skyler.rencher@gmail.com>
2020-09-12 18:02:12 -0400

    added variables in the script.js to connect the js to the html dom.

---
+++ Assets/js/script.js
@@ -0,0 +1,19 @@
+// Variables Defined
+
+// Reference to the DOM Elements
+var searchButton = $("#search-button");
+var userInputField = $("#user-search-input");
+
+var displayCurrentCity = $("#display-city");
+var currentTemp = $("#display-temp");
+var currentHumidity = $("#display-humidity");
+var currentWindSpeed = $("#display-wind-speed");
+var currentUvIndex = $("#display-uv-index");
+
+var dallasButton = $("dallas");
+var chicagoButton = $("chicago");
+var newyorkButton = $("new-york");
+var miamiButton = $("miami");
+var sanfranciscoButton = $("san-francisco");
+var seattleButton = $("seattle");
+var atlantaButton = $("atlanta");
