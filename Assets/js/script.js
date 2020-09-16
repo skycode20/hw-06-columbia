0e2107ec451d8e42ce60bbd3c4208b1863d64267
Skyler R <skyler.rencher@gmail.com>
2020-09-15 20:25:12 -0400

    changed the api url on the current day weather requests to a 6 day forecast for the respective cities so that the information can already be requested rather than being requested separately.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,36 +23,55 @@ var atlantaButton = $("#atlanta");
 var apiKey = "&key=90e9e44deac84fb4a9f46233e3d5e501";
 
 
-// Weather API - 5 Day Forecast
-var fiveDayCity = "New York, NY";
-var fiveDayURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&city=" + fiveDayCity + apiKey;
+// // Weather API - 5 Day Forecast
+// var fiveDayCity = "New York, NY";
+// var fiveDayURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city=" + fiveDayCity + apiKey;
 
-$.ajax(fiveDayURL).done(function (response) {
-	console.log(response);
-});
+// $.ajax(fiveDayURL).done(function (response) {
+// 	console.log(response);
+// });
 
 // ======================================================================
 
 // User Searched City 
-var userSearchCity = "Raleigh, NC"
-var userURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ userSearchCity + apiKey;
 
-$.ajax({
-      url: userURL,
-      method: "GET"
-    })
-    .then(function(response) {
-        // console.log(apiURL);
-        console.log(response);
-    });
-// ======================================================================
-    // Current Weather Info Function
+// var userSearchCity = $("#movie-input").val().trim();
+// var userSearchCity = $(userInputField).val().trim();
+// var citiesSearched = [""];
+// var userURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ citiesSearched + apiKey;
+
+//       // This function handles events where one button is clicked
+//     $(searchButton).on("click", function(event) {
+//         event.preventDefault();
+
+//         // This line grabs the input from the textbox
+//         // var userSearchCity = $(userInputField).val().trim();
+//         // userSearchCity.text(JSON.stringify()).push();
+        
+        
+//         citiesSearched.push(userSearchCity);
+//         console.log(citiesSearched);
+
+    
+//         });
+//         console.log(searchButton);
+
+// $.ajax({
+//       url: userURL,
+//       method: "GET"
+//     })
+//     .then(function(response) {
+//         // console.log(apiURL);
+//         console.log(response);
+//     });
+// // ======================================================================
+    // Current Weather Info Functions
     function showCurrentWeatherInfo(response) {
                 
         console.log(response);
 
         // transfer to the html
-        $(displayCurrentCity).html("<h1>" + response.data[0].city_name + " Weather Details</h1>");
+        $(displayCurrentCity).html("<h1>" + response.city_name + " Weather Details</h1>");
         $(currentTemp).text("Temperature: " + response.data[0].temp);
         $(currentWindSpeed).text("Wind Speed: " + response.data[0].wind_spd);
         $(currentHumidity).text("Humidity: " + response.data[0].rh);
@@ -64,7 +83,7 @@ $.ajax({
 
 // Dallas Weather Request 
 var dallasCity = "Dallas, TX"
-var dalURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ dallasCity + apiKey;
+var dalURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ dallasCity + apiKey;
 
     $(dallasButton).on("click", function(event) {
         event.preventDefault();
@@ -83,7 +102,7 @@ var dalURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ dallasCity 
 
 // Chicago Weather Request 
 var chicagoCity = "Chicago, IL"
-var chiURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ chicagoCity + apiKey;
+var chiURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ chicagoCity + apiKey;
 
     $(chicagoButton).on("click", function(event) {
         event.preventDefault();
@@ -102,7 +121,7 @@ var chiURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ chicagoCity
 
 // New York Weather Request 
 var newyorkCity = "New York, NY"
-var nycURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ newyorkCity + apiKey;
+var nycURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ newyorkCity + apiKey;
 
     $(newyorkButton).on("click", function(event) {
         event.preventDefault();
@@ -120,7 +139,7 @@ var nycURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ newyorkCity
 
 // Miami Weather Request 
 var miamiCity = "Miami, FL"
-var miaURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ miamiCity + apiKey;
+var miaURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ miamiCity + apiKey;
 
     $(miamiButton).on("click", function(event) {
         event.preventDefault();
@@ -139,7 +158,7 @@ var miaURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ miamiCity +
 
 // Seattle Weather Request 
 var seattleCity = "Seattle, WA"
-var seaURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ seattleCity + apiKey;
+var seaURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ seattleCity + apiKey;
 
     $(seattleButton).on("click", function(event) {
         event.preventDefault();
@@ -158,7 +177,7 @@ var seaURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ seattleCity
 
 // Atlanta Weather Request 
 var atlantaCity = "Atlanta, GA"
-var atlURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ atlantaCity + apiKey;
+var atlURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ atlantaCity + apiKey;
 
     $(atlantaButton).on("click", function(event) {
         event.preventDefault();
@@ -177,7 +196,7 @@ var atlURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ atlantaCity
 
 // San Francisco Weather Request 
 var sanfranciscoCity = "San Francisco, CA"
-var sanfranURL = "https://api.weatherbit.io/v2.0/current?units=I&city="+ sanfranciscoCity + apiKey;
+var sanfranURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=6&city="+ sanfranciscoCity + apiKey;
 
 $(sanfranciscoButton).on("click", function(event) {
     event.preventDefault();
